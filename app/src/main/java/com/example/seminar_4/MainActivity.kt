027ed6edd6d3d81698d21901bb8c388e15255ba7
amsdevelops <amsdevelops@gmail.com>
2023-09-19 01:21:05 +0300

    Template added

--- app/src/main/java/com/example/seminar_4/MainActivity.kt
+++ app/src/main/java/com/example/seminar_4/MainActivity.kt
@@ -2,10 +2,47 @@ package com.example.seminar_4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.seminar_4.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val scope = CoroutineScope(Dispatchers.IO)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+            createSpeedometerValues()
+                .onEach {
+                }
+                .launchIn(scope)
+        }
+    }
+
+    fun createSpeedometerValues() = flow {
+        var isAccelerate = true
+        var speed = 0
+        while (true) {
+            Thread.sleep(Random.nextLong(500))
+            if (isAccelerate) {
+                speed++
+                if (speed == 100) isAccelerate = false
+            } else {
+                speed--
+                if (speed == 0) isAccelerate = true
+            }
+            emit(speed)
+        }
     }
 }
